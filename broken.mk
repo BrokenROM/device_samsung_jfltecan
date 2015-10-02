48bb1dfa0b7e245a0c50840e41adf56219501127
Paul Keith <javelinanddart@gmail.com>
2015-10-02 13:16:00 -0500

    Broke it

--- broken.mk
+++ broken.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/jfltecan/full_jfltecan.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/candy5/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
 
-# Inherit some common Candy5 stuff.
-$(call inherit-product, vendor/candy5/config/common_full_phone.mk)
+# Inherit some common Broken stuff.
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltevl \
@@ -12,5 +12,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   BUILD_FINGERPRINT="samsung/jfltevl/jfltecan:4.4.2/KOT49H/I337MVLUFNC1:user/release-keys" \
   PRIVATE_BUILD_DESC="jfltevl-user 4.4.2 KOT49H I337MVLUFNC1 release-keys"
 
-PRODUCT_NAME := candy5_jfltecan
+PRODUCT_NAME := broken_jfltecan
 PRODUCT_DEVICE := jfltecan
